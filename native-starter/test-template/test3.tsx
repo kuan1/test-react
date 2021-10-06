2c1ee85d72fd5d8340b2a50573b67c3504df3f80
luzhongkuan <luzhongk@126.com>
2021-10-06 21:52:29 +0800

    fix(native): test3

---
+++ native-starter/test-template/test3.tsx
@@ -0,0 +1,37 @@
+// nfn
+// const test = (params) => {
+// }
+
+// rnstyle
+// const styles = StyleSheet.create({
+//   style
+// })
+
+// rxconst
+// export const constantName = 'constantName'
+
+// clo
+// console.log(`object`, object)
+
+// rfce
+// import React from 'react'
+// function test3() {
+//   return (
+//     <div>
+//     </div>
+//   )
+// }
+// export default test3
+
+// rnfes
+// import React from 'react'
+// import { StyleSheet, Text, View } from 'react-native'
+// const test3 = () => {
+//   return (
+//     <View>
+//       <Text></Text>
+//     </View>
+//   )
+// }
+// export default test3
+// const styles = StyleSheet.create({})
